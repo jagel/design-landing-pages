91bcc7e978fe44cf35c21f8f666b290efd410547
jagel <jagel8@hotmail.com>
2020-11-13 19:24:04 -0600

    Init template

---
+++ app.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const express = require('express');
+const path = require('path');
+const reload = require('reload');
+const open = require('open');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static('express'));
+
+app.use('/',function(req,res){
+    res.sendFile(path.join(__dirname + '/express/homepage.htm'));
+});
+
+const server = http.createServer(app);
+const port = 3000;
+
+server.listen(port);
+
+
+console.debug('Server listening on port ' + port);
+console.debug('Opening http://localhost:' + port);
