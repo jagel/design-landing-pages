9e7b4090c30c1b46583712547bce6cc454672403
jagel <jagel8@hotmail.com>
2020-11-13 23:39:31 -0600

    Index page created

--- app.js
+++ app.js
@@ -10,14 +10,22 @@ app.use(express.json());
 app.use(express.static('src'));
 
 app.use('/',function(req,res){
-    res.sendFile(path.join(__dirname + '/src/index.htm'));
+    res.sendFile(path.join(__dirname + '/src/index.html'));
 });
 
 const server = http.createServer(app);
 const port = 3000;
 
-server.listen(port);
+reload(app).then(function (reloadReturned) {
+    server.listen(port, function () {
+      console.log('Web server listening on port ' + port)
+      console.log('http://localhost:' + port)
+    })
+  }).catch(function (err) {
+    console.error('Reload could not start, could not start server/sample app', err)
+})
 
 
 console.debug('Server listening on port ' + port);
 console.debug('Opening http://localhost:' + port);
+
